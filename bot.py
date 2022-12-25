8db7e579d9ecfa3afaa813deb3bfcf1d05841172
Anu00123 <97594827+Anu00123@users.noreply.github.com>
2022-12-25 15:56:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ async def start(client: Anu00123, message: Message):
       ]]
     await message.reply_text(text="**𝙷𝙴𝙻𝙻𝙾...⚡\n\n𝙸𝙰𝙼 𝙰 𝚂𝙸𝙼𝙿𝙻𝙴 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙰𝚄𝚃𝙾 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝙰𝙲𝙲𝙴𝙿𝚃 𝙱𝙾𝚃.\n𝙵𝙾𝚁 𝚈𝙾𝚄𝚁 𝙲𝙷𝙰𝚃𝚂 𝙲𝚁𝙴𝙰𝚃𝙴 𝙾𝙽𝙴 𝙱𝙾𝚃... \n𝚅𝙸𝙳𝙴𝙾 𝙾𝙽 𝙼𝚈 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻**", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
-
 @Anu00123.on_chat_join_request(filters.chat(CHAT_ID))
 async def autoapprove(client: Anu00123, message: ChatJoinRequest):
     chat=message.chat # Chat
